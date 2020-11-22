c68ae543324e92dc3a442a8560871aca3c4ff0d4
Delandmeter Yannick <delandmeter.yannick@student.ucll.be>
2020-11-22 15:17:23 +0100

    Dns: updated dns add zone script

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -9,26 +9,27 @@ then
 else
 
 	# add ns record
-	echo "\nzone $ZONE.yannick-delandmeter.sb.uclllabs.be. {
+	echo "" >> /etc/bind/named.conf.mrt-zones
+	echo "zone $ZONE.yannick-delandmeter.sb.uclllabs.be. {
 		type master;
 		file \"/etc/bind/zones/db.mrt.$ZONE.yannick-delandmeter.sb.uclllabs.be\";
 	};" >> /etc/bind/named.conf.mrt-zones
 
 	# create file
-	echo "\$TTL	300
-	@	IN	SOA	ns.$ZONE.yannick-delandmeter.sb.uclllabs.be. root.yannick-delandmeter.sb.uclllabs.be. (
-			      1		; Serial
-			    300		; Refresh
-			  86400		; Retry
-			2419200		; Expire
-			  86400)	; Negative Cache TTL
+	echo "
+\$TTL	300
+@	IN	SOA	ns.$ZONE.yannick-delandmeter.sb.uclllabs.be. root.yannick-delandmeter.sb.uclllabs.be. (
+		      1		; Serial
+		    300		; Refresh
+		  86400		; Retry
+		2419200		; Expire
+		  86400)	; Negative Cache TTL
+; Name Servers
+@	IN	NS	ns.$ZONE.yannick-delandmeter.sb.uclllabs.be.
+@	IN	NS	ns.yannick-delandmeter.sb.uclllabs.be.
 
-	; Name Servers
-	@	IN	NS	ns.$ZONE.yannick-delandmeter.sb.uclllabs.be.
-	@	IN	NS	ns.yannick-delandmeter.sb.uclllabs.be.
-
-	; A records
-	@	IN	A	193.191.177.223
-	ns	IN	A	193.191.177.223" >> $(echo "/etc/bind/zones/db.mrt.$ZONE.yannick-delandmeter.sb.uclllabs.be")
+; A records
+@	IN	A	193.191.177.223
+ns	IN	A	193.191.177.223" >> $(echo "/etc/bind/zones/db.mrt.$ZONE.yannick-delandmeter.sb.uclllabs.be")
 
 fi
