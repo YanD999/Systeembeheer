7097c38c3904c19d5364436ef5c906ccb4ea0445
Delandmeter Yannick <delandmeter.yannick@student.ucll.be>
2020-11-22 15:23:21 +0100

    Dns script: updated layout

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -19,11 +19,11 @@ else
 	echo "
 \$TTL	300
 @	IN	SOA	ns.$ZONE.yannick-delandmeter.sb.uclllabs.be. root.yannick-delandmeter.sb.uclllabs.be. (
-		      1		; Serial
-		    300		; Refresh
-		  86400		; Retry
-		2419200		; Expire
-		  86400)	; Negative Cache TTL
+			      1		; Serial
+			    300		; Refresh
+			  86400		; Retry
+			2419200		; Expire
+			  86400)	; Negative Cache TTL
 ; Name Servers
 @	IN	NS	ns.$ZONE.yannick-delandmeter.sb.uclllabs.be.
 @	IN	NS	ns.yannick-delandmeter.sb.uclllabs.be.
