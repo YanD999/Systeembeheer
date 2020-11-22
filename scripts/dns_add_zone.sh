683305234d2dec93e925ce34a4c3bb13ae9ce510
Delandmeter Yannick <delandmeter.yannick@student.ucll.be>
2020-11-22 14:13:06 +0100

    Dns: Update script

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
 # zone name
 ZONE=$1
-// todo check not empty
 
-# add ns record
-echo "\nzone $ZONE.yannick-delandmeter.sb.uclllabs.be. {
-	type master;
-	file \"/etc/bind/zones/db.mrt.$ZONE.yannick-delandmeter.sb.uclllabs.be\";
-};" >> /etc/bind/named.conf.mrt-zones
+# check empty
+if [ -z "$ZONE" ]
+then
+	echo "Geen naam gegeven"
+else
+
+	# add ns record
+	echo "\nzone $ZONE.yannick-delandmeter.sb.uclllabs.be. {
+		type master;
+		file \"/etc/bind/zones/db.mrt.$ZONE.yannick-delandmeter.sb.uclllabs.be\";
+	};" >> /etc/bind/named.conf.mrt-zones
+
+	# create file
+
+fi
